2512d69a2075167f40c7a629f7bfa26782c13f94
Vladyslav Kolodka <vladkolodka@gmail.com>
2017-11-18 01:31:07 +0200

    SignalR client libraries

--- App.js
+++ App.js
@@ -1,37 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, Alert } from 'react-native';
+import { HttpConnection, HubConnection } from '@aspnet/signalr-client';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default class App extends Component<{}> {
+    this.connection = new HubConnection(new HttpConnection("http://10.0.2.2:12345/chat"));
+
+    this.state = {
+      messages: [],
+      message: '',
+      error: null
+    };
+
+    this.onSend = this.onSend.bind(this);
+    this.addMessage = this.addMessage.bind(this);
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.error !== null && prevState.error === null) {
+      Alert.alert("Error", this.state.error, [
+        { text: 'Ok' }
+      ]);
+
+      this.setState({ error: null });
+    }
+  }
+
+
+
+  addMessage(message) {
+    this.setState({
+      messages: [...this.state.messages,
+      { key: this.state.messages.length, message: message }
+      ]
+    });
+  }
+
+  componentDidMount() {
+    this.connection.on('Send', data => this.addMessage(data));
+
+    this.connection.start().catch(_ => {
+      this.setState({ error: "Connection can not be established!" });
+    });
+  }
+
+  onSend() {
+    this.connection.invoke("Send", this.state.message);
+
+    this.setState({ message: '' });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FlatList data={this.state.messages} style={styles.container}
+          renderItem={({ item }) =>
+            <Text style={styles.row}>{item.message}</Text>
+          } />
+
+        <TextInput value={this.state.message} onChangeText={text => this.setState({ message: text })} />
+        <Button title='Send' onPress={this.onSend} />
       </View>
     );
   }
@@ -39,19 +69,12 @@ export default class App extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  row: {
+    marginVertical: 2,
+    backgroundColor: 'silver',
+    color: 'red',
+    padding: 5
+  }
 });
