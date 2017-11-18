7e8a2dc1b7da266876e525ed9bbfdcdbe136f468
Vladyslav Kolodka <vladkolodka@gmail.com>
2017-11-18 18:21:04 +0200

    Broadcaster

--- HelloSignalR/HelloSignalR/Hubs/ChatHub.cs
+++ HelloSignalR/HelloSignalR/Hubs/ChatHub.cs
@@ -1,10 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace HelloSignalR.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Timer _broadcaster;
+
+        private readonly IHubContext<ChatHub> _context;
+
+        public ChatHub(IHubContext<ChatHub> context)
+        {
+            if (_broadcaster != null) return;
+
+            _context = context;
+            _broadcaster = new Timer(Tick, null, 0, 1000);
+        }
+
+        private void Tick(object state)
+        {
+            _context.Clients.All.InvokeAsync("Send", $"[Server] {DateTime.Now}");
+        }
+
         public async Task Send(string message)
         {
             await Clients.All.InvokeAsync("Send", message);
